c338724f69c40da5547e318917f043aeb2ba0598
leon <leon@1020493635@qq.com>
2023-08-17 22:43:19 +0800

    mutual use position config and excel writer

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from input_parser import MeetingParser, ParentEvent, NoticeEvent
+from config_reader import PositionCalculator
 import xlsx_writer as xw
+from excel_utils import add_coordinates
 
 if __name__=='__main__':
     parser = MeetingParser()
     parser.parse_file("/home/didi/myproject/tmma/user_input.txt")
     event_list = parser.event_list
 
+    # TODO: Move cur_time to input.txt
     cur_time = '15:00'
     for parent_event in event_list:
         parent_event.calculate_time(cur_time)
         cur_time = parent_event.get_end_time()
 
+    pc = PositionCalculator('/home/didi/myproject/tmma/config.yaml')
+    pc.set_schedule_block_height(43)
+    start_coord_dict = pc.get_start_coords()
+    # print(start_coord_dict)
+    # print(pc.calculate_sizes())
 
     template_file = '/home/didi/myproject/tmma/tm_303_calendar.xlsx'
+    template_sheet_name = 'calendar'
     target_file = '/home/didi/myproject/tmma/generated_calendar.xlsx'
     target_sheet_name = 'Sheet'
     image_path = '/home/didi/myproject/tmma/tm_logo.jpg'
 
-    title_block = 'title_block'
     theme_block = 'theme_block'
     parent_event_template = 'parent_event'
     child_event_template = 'child_event'
@@ -25,20 +33,29 @@ if __name__=='__main__':
     rule_block = 'rule_block'
     information_block = 'information_block'
 
-    xlsx_writer = xw.XlsxWriter(template_file,target_file,target_sheet_name)
-    xlsx_writer.write_sheet('title_block')
-    xlsx_writer.write_sheet('theme_block',start_coord='B9',data={'{theme_name}':'主题：父亲节','{time}':'15:00','{organizer_name}':'林长虹','{SAA_name}':'Leon Lin'})
-    cur_start_coord = 'B12'
+    # TODO: Read template position config from config.yaml.
+    template_position_config = {'title_block': {'start_coord':'B2','end_coord':'O8'},'theme_block':{'start_coord':'B9','end_coord':'K11'},'parent_block':{'start_coord':'B12','end_coord':'K12'},'child_block':{'start_coord':'B13','end_coord':'K13'},'notice_block':{'start_coord':'B49','end_coord':'K49'},'rule_block':{'start_coord':'L9','end_coord':'O15'},'information_block':{'start_coord':'L33','end_coord':'O54'},'contact_block':{'start_coord':'L16','end_coord':'O24'},'project_block':{'start_coord':'L25','end_coord':'O32'}}
+
+    xlsx_writer = xw.XlsxWriter(template_file,template_sheet_name,target_file,target_sheet_name)
+    # TODO: save templae position config as private member of writer.
+    xlsx_writer.write_sheet(source_position=template_position_config['title_block'],target_start_coord=start_coord_dict['title_block'])
+    # TODO：Parse theme from input.txt
+    xlsx_writer.write_sheet(source_position=template_position_config['theme_block'],target_start_coord=start_coord_dict['theme_block'],data={'{theme_name}':'主题：父亲节','{time}':'15:00','{organizer_name}':'林长虹','{SAA_name}':'Leon Lin'})
+    cur_start_coord = start_coord_dict['schedule_block']
     for event in event_list:
-        cur_start_coord = xw._add_coordinates(cur_start_coord,(0,1))
         if isinstance(event,NoticeEvent):
-            xlsx_writer.write_sheet(notice_event_template,start_coord=cur_start_coord,data=event.get_event())
+            xlsx_writer.write_sheet(source_position=template_position_config['notice_block'],target_start_coord=cur_start_coord,data=event.get_event())
+            cur_start_coord = add_coordinates(cur_start_coord,(0,1))
         elif isinstance(event,ParentEvent):
-            xlsx_writer.write_sheet(parent_event_template,start_coord=cur_start_coord,data=event.get_parent_event())
+            xlsx_writer.write_sheet(source_position=template_position_config['parent_block'],target_start_coord=cur_start_coord,data=event.get_parent_event())
+            cur_start_coord = add_coordinates(cur_start_coord,(0,1))
             for child_event in event.get_child_events():
-                cur_start_coord = xw._add_coordinates(cur_start_coord,(0,1))
-                xlsx_writer.write_sheet(child_event_template,start_coord=cur_start_coord,data=child_event)
-    xlsx_writer.write_sheet('rule_block',start_coord='L9')
-    xlsx_writer.write_sheet('information_block',start_coord='L16')
+                xlsx_writer.write_sheet(source_position=template_position_config['child_block'],target_start_coord=cur_start_coord,data=child_event)
+                cur_start_coord = add_coordinates(cur_start_coord,(0,1))
+    xlsx_writer.write_sheet(source_position=template_position_config['contact_block'],target_start_coord=start_coord_dict['contact_block'])
+    xlsx_writer.write_sheet(source_position=template_position_config['project_block'],target_start_coord=start_coord_dict['project_block'])
+    xlsx_writer.write_sheet(source_position=template_position_config['rule_block'],target_start_coord=start_coord_dict['rule_block'])
+    xlsx_writer.write_sheet(source_position=template_position_config['information_block'],target_start_coord=start_coord_dict['information_block'])
+    # TODO: make image coord configable.
     xlsx_writer.add_image(image_path, target_cell_coord='B2')
     xlsx_writer.save()
--- xlsx_writer.py
+++ xlsx_writer.py
@@ -11,8 +11,9 @@ from excel_utils import coordinate_string_to_index,add_coordinates,get_left_top_
 
 
 class XlsxWriter():
-    def __init__(self,template_xlsx_file_path,target_xlsx_file_path,target_sheet_name):
+    def __init__(self,template_xlsx_file_path,template_sheet_name,target_xlsx_file_path,target_sheet_name):
         self.template_workbook = load_workbook(template_xlsx_file_path)
+        self.template_sheet = self.template_workbook[template_sheet_name]
         self.target_workbook = Workbook()  # Create a new workbook if the target doesn't exist
         self.target_xlsx_file_path = target_xlsx_file_path
         # Create or get the sheet with the desired sheet name
@@ -26,11 +27,11 @@ class XlsxWriter():
         self.target_workbook.close()  # 关闭文件
         self.template_workbook.close()
 
-    def write_sheet(self,source_sheet_name,source_start_coord=None,source_end_coord=None,target_start_coord='A1',data=None):
-        src_file_sheet = self._copy_sheet_impl(self.template_workbook[source_sheet_name],source_start_coord,source_end_coord)
+    def write_sheet(self,source_position=None,target_start_coord='A1',data=None):
+        src_file_sheet = self._copy_sheet_impl(self.template_sheet,source_position)
         if data is not None:
             src_file_sheet = self._modify_sheet(src_file_sheet,data)
-        self._paste_sheet_impl(self.target_sheet,target_start_coord,src_file_sheet,source_start_coord,source_end_coord)
+        self._paste_sheet_impl(self.target_sheet,target_start_coord,src_file_sheet,source_position)
         
     def add_image(self,image_path, target_cell_coord,image_width=100,image_height=100):
         # Load the image
@@ -71,11 +72,13 @@ class XlsxWriter():
         coord2_col_idx,coord2_row_idx = coordinate_string_to_index(coord2)
         return (coord1_col_idx-coord2_col_idx,coord1_row_idx-coord2_row_idx)
 
-    def _copy_sheet_impl(self,original_sheet,start_coord=None,end_coord=None):
-        if start_coord is None:
+    def _copy_sheet_impl(self,original_sheet,source_position=None):
+        if source_position is None:
             start_coord = get_left_top_coordinate(original_sheet)
-        if end_coord is None:
             end_coord = get_right_bottom_coordinate(original_sheet)
+        else:
+            start_coord = source_position['start_coord']
+            end_coord = source_position['end_coord']
 
         new_workbook = Workbook()
         new_workbook.remove(new_workbook.active)  # Remove the default sheet
@@ -122,11 +125,13 @@ class XlsxWriter():
     
         return new_sheet
 
-    def _paste_sheet_impl(self,target_file_sheet,target_start_coord,src_file_sheet,src_start_coord=None,src_end_coord=None):
-        if src_start_coord is None:
+    def _paste_sheet_impl(self,target_file_sheet,target_start_coord,src_file_sheet,source_position=None):
+        if source_position is None:
             src_start_coord = get_left_top_coordinate(src_file_sheet)
-        if src_end_coord is None:
             src_end_coord = get_right_bottom_coordinate(src_file_sheet)
+        else:
+            src_start_coord = source_position['start_coord']
+            src_end_coord = source_position['end_coord']
 
         shift_offset = self._subtract_coordinates(target_start_coord,src_start_coord)
         start_col, start_row = coordinate_string_to_index(src_start_coord)
@@ -187,31 +192,31 @@ if __name__=='__main__':
     # res = add_coordinates('B12',(0,1))
 
     template_file = '/home/didi/myproject/tmma/tm_303_calendar.xlsx'
+    template_sheet_name = 'calendar'
     target_file = '/home/didi/myproject/tmma/generated_calendar.xlsx'
     target_sheet_name = 'Sheet'
     image_path = '/home/didi/myproject/tmma/tm_logo.jpg'
 
     title_block = 'title_block'
     theme_block = 'theme_block'
-    parent_event = 'parent_event'
-    child_event = 'child_event'
+    parent_block = 'parent_block'
+    child_block = 'child_block'
     notice_block = 'notice_block'
     rule_block = 'rule_block'
     information_block = 'information_block'
 
-    template_block_config = {'title_block': {'start_coord':'B2','end_coord':'O8'},'theme_block':{'start_coord':'B9','end_coord':'K11'}}
+    template_position_config = {'title_block': {'start_coord':'B2','end_coord':'O8'},'theme_block':{'start_coord':'B9','end_coord':'K11'},'parent_block':{'start_coord':'B12','end_coord':'K12'},'child_block':{'start_coord':'B13','end_coord':'K13'}}
 
-    xlsx_writer = XlsxWriter(template_file,target_file,target_sheet_name)
-    xlsx_writer.write_sheet('calendar',source_start_coord=template_block_config[title_block]['start_coord'],source_end_coord=template_block_config[title_block]['end_coord'],target_start_coord='B2')
-    xlsx_writer.write_sheet('calendar',source_start_coord=template_block_config[theme_block]['start_coord'],source_end_coord=template_block_config[theme_block]['end_coord'],target_start_coord='B9',data={'{theme_name}':'主题：父亲节','{time}':'15:00','{organizer_name}':'林长虹','{SAA_name}':'Leon Lin'})
-    # xlsx_writer.write_sheet('theme_block',target_start_coord='B9',data={'{theme_name}':'主题：父亲节','{time}':'15:00','{organizer_name}':'林长虹','{SAA_name}':'Leon Lin'})
-    # cur_start_coord = 'B12'
-    # for i in range(3):
-    #     cur_start_coord = add_coordinates(cur_start_coord,(0,1))
-    #     xlsx_writer.write_sheet(parent_event,target_start_coord=cur_start_coord,data={'{start_time}':'12:01','{event_name}':'开场','{duration}':'8'})
-    #     cur_start_coord = add_coordinates(cur_start_coord,(0,1))
-    #     xlsx_writer.write_sheet(child_event,target_start_coord=cur_start_coord,data={'{event_name}':'开场白','{end_time}':'15:01','{duration}':'8','{host_name}':'林长宏'})
-    # xlsx_writer.write_sheet('rule_block',target_start_coord='L9')
-    # xlsx_writer.write_sheet('information_block',target_start_coord='L16')
+    xlsx_writer = XlsxWriter(template_file,template_sheet_name,target_file,target_sheet_name)
+    xlsx_writer.write_sheet(source_position=template_position_config[title_block],target_start_coord='B2')
+    xlsx_writer.write_sheet(source_position=template_position_config[theme_block],target_start_coord='B9',data={'{theme_name}':'主题：父亲节','{time}':'15:00','{organizer_name}':'林长虹','{SAA_name}':'Leon Lin'})
+    cur_start_coord = 'B12'
+    for i in range(3):
+        cur_start_coord = add_coordinates(cur_start_coord,(0,1))
+        xlsx_writer.write_sheet(source_position=template_position_config[parent_block],target_start_coord=cur_start_coord,data={'{start_time}':'12:01','{event_name}':'开场','{duration}':'8'})
+        cur_start_coord = add_coordinates(cur_start_coord,(0,1))
+        xlsx_writer.write_sheet(source_position=template_position_config[child_block],target_start_coord=cur_start_coord,data={'{event_name}':'开场白','{end_time}':'15:01','{duration}':'8','{host_name}':'林长宏'})
+    # xlsx_writer.write_sheet('calendar',source_position=template_position_config[rule_block],target_start_coord='L9')
+    # xlsx_writer.write_sheet('calendar',source_position=template_position_config[information_block],target_start_coord='L16')
     xlsx_writer.add_image(image_path, target_cell_coord='B2')
     xlsx_writer.save()
